cdf5fad841ce2436b1e25649c5fbcac7dc39e533
rovilan-pro <rovilec143@gmail.com>
2025-06-02 13:41:06 +0800

    Updated Ordering

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Address address1 = new Address("123 Mabini St", "Navotas City", "Metro Manila", "Philippines");
-        Customer customer1 = new Customer("Juan Dela Cryz", address1);
+        Customer customer1 = new Customer("Juan Dela Cruz", address1);
 
         Order order1 = new Order(customer1);
         order1.AddProduct(new Product("Mouse", "P001", 900, 1));
@@ -13,6 +13,19 @@ class Program
 
         Console.WriteLine(order1.GetPackingLabel());
         Console.WriteLine(order1.GetShippingLabel());
-        Console.WriteLine($"Total Cost: ${order1.GetTotalCost():0.00}");
+        Console.WriteLine($"Total Cost: P{order1.GetTotalCost():0.00}");
+
+        Console.WriteLine();
+
+        Address address2 = new Address("123 Del Pilar St", "Cabanatuan City", "Nueva Ecija", "Philippines");
+        Customer customer2 = new Customer("Rizal Juanez", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Key Chain", "P0010", 25, 100));
+        order2.AddProduct(new Product("Keyboard", "P0011", 999.99, 1));
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: P{order2.GetTotalCost():0.00}");
     }
 }
