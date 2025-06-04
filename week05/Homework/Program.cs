8ccaa01b9011c1580ceb24eb6d9b1a1d5bac0971
rovilan-pro <rovilec143@gmail.com>
2025-06-04 12:07:29 +0800

    Homework Done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assign1 = new Assignment("Mike Mikeson", "Algebra");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment assign2 = new MathAssignment("Arnold Mathman", "Fraction", "7.3", "8-19");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign2.GetHomeworkList());
+
+        WritingAssignment assign3 = new WritingAssignment("Mary Waters", "European History", "The Cause of World War II");
+        Console.WriteLine(assign3.GetSummary());
+        Console.WriteLine(assign3.GetWritingInformation());
     }
 }
